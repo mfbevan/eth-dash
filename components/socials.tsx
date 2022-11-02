23873105abfd118f8c26d00c12fa55b7345a9f99
mfbevan <matthewbevan00@gmail.com>
2022-11-03 00:04:53 +1000

    refactor: 💄 tooltip styling

--- components/socials.tsx
+++ components/socials.tsx
@@ -4,7 +4,7 @@ import { socials } from "../constants";
 export const Socials = () => (
   <HStack spacing="30px">
     {socials.map(({ label, link, icon }) => (
-      <Tooltip key={label} label={label}>
+      <Tooltip key={label} label={label} rounded="md">
         <Link href={link} target="_blank">
           {icon}
         </Link>
