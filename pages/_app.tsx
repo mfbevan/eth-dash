112da46b0a607e3e59cef17c9d0fae9c74baa634
mfbevan <matthewbevan00@gmail.com>
2022-11-03 01:30:41 +1000

    feat: 📈 analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import { theme } from "../styles/theme";
 import { Metadata } from "../providers";
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ChakraProvider theme={theme}>
       <Metadata />
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
