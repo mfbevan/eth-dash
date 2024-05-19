e3ff6c0bb6346c8b2b3f03626edde3e745502bd5
mfbevan <matthewbevan00@gmail.com>
2024-05-19 16:31:25 +1000

    feat: ✨ moving link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,28 @@
-import { useColorModeValue, Button, Flex } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import {
+  useColorModeValue,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  useDisclosure,
+  CloseButton,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 import Link from "next/link";
 import { ImChrome } from "react-icons/im";
 
 import { PageContainer } from "../components";
-import {
-  AbsoluteButtonContainer,
-  DarkModeButton,
-} from "../components/dark-mode";
 import { Dashboards } from "../components/dashboards";
 import { Socials } from "../components/socials";
 import { InlineLink, PageSubtitle, PageTitle } from "../components/text";
 import { poppinsBody } from "../styles/fonts";
 
 export default function Home() {
+  const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true });
   return (
     <PageContainer className={poppinsBody.className}>
       <PageTitle>Ethereum Dashboards</PageTitle>
@@ -32,6 +42,27 @@ export default function Home() {
         </Link>
       </Flex>
       <Dashboards />
+
+      <Modal isOpen={isOpen} isCentered onClose={onClose}>
+        <ModalOverlay backdropFilter="blur(10px) hue-rotate(90deg)" />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalHeader>We have moved!</ModalHeader>
+          <ModalBody>
+            <Flex flexDirection="column" alignItems="center" gap={2} pb={4}>
+              We&apos;ve got a new look! ethdash.xyz has been merged with
+              explorer.nexeth.xyz. You can now find all of your favourite
+              Ethereum Dashboards in one place, along with the new Nexeth
+              Explorer.
+              <Link href="https://explorer.nexeth.xyz/dashboards">
+                <Button rightIcon={<ExternalLinkIcon />}>
+                  Visit Nexeth Explorer
+                </Button>
+              </Link>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </PageContainer>
   );
 }
