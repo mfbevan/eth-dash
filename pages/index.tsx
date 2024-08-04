d97e36a2d967afe9816eb0cd38d0663f7310ebaa
mfbevan <matthewbevan00@gmail.com>
2024-08-04 15:15:38 +1000

    refactor: ♻️ remove moving modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,4 @@
-import { ExternalLinkIcon } from "@chakra-ui/icons";
-import {
-  useColorModeValue,
-  Button,
-  Flex,
-  Modal,
-  ModalBody,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  useDisclosure,
-  CloseButton,
-  ModalCloseButton,
-} from "@chakra-ui/react";
+import { useColorModeValue, Button, Flex } from "@chakra-ui/react";
 import Link from "next/link";
 import { ImChrome } from "react-icons/im";
 
@@ -22,7 +9,6 @@ import { InlineLink, PageSubtitle, PageTitle } from "../components/text";
 import { poppinsBody } from "../styles/fonts";
 
 export default function Home() {
-  const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true });
   return (
     <PageContainer className={poppinsBody.className}>
       <PageTitle>Ethereum Dashboards</PageTitle>
@@ -42,27 +28,6 @@ export default function Home() {
         </Link>
       </Flex>
       <Dashboards />
-
-      <Modal isOpen={isOpen} isCentered onClose={onClose}>
-        <ModalOverlay backdropFilter="blur(10px) hue-rotate(90deg)" />
-        <ModalContent>
-          <ModalCloseButton />
-          <ModalHeader>We have moved!</ModalHeader>
-          <ModalBody>
-            <Flex flexDirection="column" alignItems="center" gap={2} pb={4}>
-              We&apos;ve got a new look! ethdash.xyz has been merged with
-              explorer.nexeth.xyz. You can now find all of your favourite
-              Ethereum Dashboards in one place, along with the new Nexeth
-              Explorer.
-              <Link href="https://explorer.nexeth.xyz/dashboards">
-                <Button rightIcon={<ExternalLinkIcon />}>
-                  Visit Nexeth Explorer
-                </Button>
-              </Link>
-            </Flex>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
     </PageContainer>
   );
 }
